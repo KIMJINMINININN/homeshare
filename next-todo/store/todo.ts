623f0219d72474d2b42b5b64e963ebb02cddd4d9
KIMJINMINININN <vmelove@naver.com>
2022-02-13 22:50:02 +0900

    store apply -ing

--- next-todo/store/todo.ts
+++ next-todo/store/todo.ts
@@ -1,1 +1,34 @@
-import {TodoType} from "../types/todo";
+import { TodoType } from "../types/todo";
+
+// action type
+export const INIT_TODO_LIST = "todo/INIT_TODO_LIST";
+
+// action 생성자
+export const setTodo = (payload: TodoType[]) => {
+    return {
+        type: INIT_TODO_LIST,
+        payload,
+    };
+};
+
+export const todoActions = { setTodo };
+
+interface TodoReduxState {
+    todos: TodoType[];
+}
+
+// 초기상태
+const initialState: TodoReduxState = {
+    todos: [],
+}
+
+//reducer
+export default function reducer(state = initialState, action: any) {
+    switch (action.type) {
+        case INIT_TODO_LIST:
+            const newState = { ...state, todos: action.payload };
+            return newState;
+        default:
+            return state;
+    }
+}
